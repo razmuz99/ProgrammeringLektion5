ba16db0f74b7993568102f66dead02d1b97f4994
Jens Daniel Kramhøft <jdkramhoft@gmail.com>
2018-10-05 08:48:28 +0200

    Added description of exercise 2. Refactored file name to match criteria.

--- Exercises/Cars.java
+++ Exercises/Cars.java
@@ -1,4 +1,13 @@
 package Exercises;
 
+/*
+Opgave 6.2 - Lav en klasse Car, der repræsenterer oplysninger om en bil og har metoder:
+        - Oplysning: Mærke (f.eks. "Citroën")
+        - Oplysning: Nummerplade (f.eks. "SV 27698")
+        - Oplysning: Farve (f.eks. "rød")
+        Metode: En toString() metode der formaterer oplysningerne som en tegnstreg.
+        Metode: Undersøg om en bil er samme mærke som en anden bil.
+*/
+
 public class Cars {
 }
